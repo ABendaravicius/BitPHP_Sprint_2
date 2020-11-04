81b7df99456755ce28081fb0c000f95aa6e71a8f
Arnas Bendaravičius <arnasbendaravicius@gmail.com>
2020-11-04 15:21:39 +0200

    added css glitter

--- index.php
+++ index.php
@@ -20,16 +20,24 @@
 <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <a href="./?path=projects">Projects</a><br>
-    <a href="./?path=people">People</a>
+    <header>
+        <div class="header container">
+            <div class="link-wrap">
+                <a href="./?path=people">People</a>
+                <a href="./?path=projects">Projects</a><br>
+            </div>
+            <h1><?php $path = $_GET['path'] ?? 'people'; echo $path; ?></h1>  
+        </div>
+    </header>
 
-    <?php
-        $path = $_GET['path'] ?? 'people';
-        if ($path == 'people') {
-            include 'utils/people.php'; // Loads people data
-        } else if ($path == 'projects') {
-            include 'utils/projects.php'; // Loads projects data
-        }
-    ?>
+    <div class="container">
+        <?php
+            if ($path == 'people') {
+                include 'utils/people.php'; // Loads people data
+            } else if ($path == 'projects') {
+                include 'utils/projects.php'; // Loads projects data
+            }
+        ?>
+    </div>
 </body>
 </html>
